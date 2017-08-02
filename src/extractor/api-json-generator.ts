2b585f1e319e673cf5fa23f501fad1cb1778a09f
Martynas Žilinskas <martiogalaltu@gmail.com>
2017-08-02 17:02:03 +0300

    Fixed ApiJsonGenerator: params.

--- src/extractor/api-json-generator.ts
+++ src/extractor/api-json-generator.ts
@@ -1,12 +1,51 @@
 import { ApiJsonGenerator as Generator, Extractor } from "@microsoft/api-extractor";
-import { ExtractedApiJson } from "./api-json-contracts";
+import ApiParameter from "@microsoft/api-extractor/lib/definitions/ApiParameter";
+import ApiFunction from "@microsoft/api-extractor/lib/definitions/ApiFunction";
+import { ReleaseTag } from "@microsoft/api-extractor/lib/definitions/ApiDocumentation";
+import ApiJsonFile from "@microsoft/api-extractor/lib/generators/ApiJsonFile";
+import { IReturn, IParam } from "@microsoft/api-extractor/lib/IDocElement";
+
+import { ApiJson } from "./api-json-contracts";
 
 export class ApiJsonGenerator extends Generator {
-    public GetFileContents(extractor: Extractor): ExtractedApiJson {
+    public GetFileContents(extractor: Extractor): ApiJson {
         this.visit(extractor.package, this.jsonOutput);
 
         // TODO: Add JSON validation scheme.
 
-        return this.jsonOutput as ExtractedApiJson;
+        return this.jsonOutput as ApiJson;
+    }
+
+    protected visitApiFunction(apiFunction: ApiFunction, refObject?: { [key: string]: any }): void {
+        if (!apiFunction.supportedName) {
+            return;
+        }
+
+        const parameters: { [key: string]: IParam } = {};
+
+        for (const param of apiFunction.params) {
+            // FIXME: any
+            parameters[param.name] = {} as any;
+
+            this.visitApiParam(param, parameters[param.name]);
+        }
+        const returnValueNode: IReturn = {
+            type: apiFunction.returnType,
+            description: apiFunction.documentation.returnsMessage
+        };
+
+        const newNode: Object = {
+            kind: ApiJsonFile.convertKindToJson(apiFunction.kind),
+            returnValue: returnValueNode,
+            parameters: parameters,
+            deprecatedMessage: apiFunction.documentation.deprecatedMessage || [],
+            summary: apiFunction.documentation.summary || [],
+            remarks: apiFunction.documentation.remarks || [],
+            isBeta: apiFunction.documentation.releaseTag === ReleaseTag.Beta
+        };
+
+        if (refObject != null) {
+            refObject[apiFunction.name] = newNode;
+        }
     }
 }
