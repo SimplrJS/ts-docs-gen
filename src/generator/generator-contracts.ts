f8f9cc70932d73e810095ba1776a63172f0b0966
Martynas Zilinskas <martiogalaltu@gmail.com>
2017-11-15 19:11:41 +0200

    Removed all files and dependencies for rewriting.

--- src/generator/generator-contracts.ts
+++
@@ -1,6 +0,0 @@
-import * as json2md from "json2md";
-
-export interface Table extends json2md.DefaultConverters.TableInput {
-    headers: string[];
-    rows: string[][];
-}
--- test.md
+++
@@ -1,2 +0,0 @@
-# Package name
-## Interfaces
