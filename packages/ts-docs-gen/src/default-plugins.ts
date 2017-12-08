da0b15a2842e110cf73c7b014d05cdfc21b8df86
Martynas Zilinskas <martiogalaltu@gmail.com>
2017-12-08 18:02:24 +0200

    Added plugin to default plugins list.

--- packages/ts-docs-gen/src/default-plugins.ts
+++ packages/ts-docs-gen/src/default-plugins.ts
@@ -1,7 +1,9 @@
 import { ApiVariablePlugin } from "./plugins/api-variable-plugin";
 import { ApiEnumPlugin } from "./plugins/api-enum-plugin";
+import { ApiTypePlugin } from "./plugins/api-type-plugin";
 
 export const DefaultPlugins = [
     new ApiVariablePlugin(),
-    new ApiEnumPlugin()
+    new ApiEnumPlugin(),
+    new ApiTypePlugin()
 ];
