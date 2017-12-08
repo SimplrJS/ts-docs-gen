684e6536d4c5959c42e4cd1fa0a90a2eb6bdbafc
Deividas Bakanas <deividas@quatrodev.com>
2017-12-08 18:33:08 +0200

    Default plugin added to DefaultPlugins.

--- packages/ts-docs-gen/src/default-plugins.ts
+++ packages/ts-docs-gen/src/default-plugins.ts
@@ -1,7 +1,9 @@
 import { ApiVariablePlugin } from "./plugins/api-variable-plugin";
 import { ApiEnumPlugin } from "./plugins/api-enum-plugin";
+import { ApiFunctionPlugin } from "./plugins/api-function-plugin";
 
 export const DefaultPlugins = [
     new ApiVariablePlugin(),
-    new ApiEnumPlugin()
+    new ApiEnumPlugin(),
+    new ApiFunctionPlugin()
 ];
