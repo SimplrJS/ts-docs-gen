a43972ade812807c4e0083b60ce2e7fc8c06155b
Donatas Bražionis <donatas@quatrodev.com>
2017-12-12 17:57:24 +0200

    Updated tests-generator.

--- tools/test-generator-cli/src/tests-generator.ts
+++ tools/test-generator-cli/src/tests-generator.ts
@@ -1,15 +1,15 @@
-import * as os from "os";
 import * as path from "path";
 import * as fs from "fs-extra";
 import fastGlob from "fast-glob";
+import * as handlebars from "handlebars";
 
 import {
     FixSep,
-    Tab,
     GENERATED_TESTS_DIR_NAME,
     TESTS_CONFIG_FILE_NAME,
     DEFAULT_TEMPLATE_FILE_NAME,
-    CASE_TEMPLATE_FILE_NAME
+    CASE_TEMPLATE_FILE_NAME,
+    RegisterJSONStringifyHandlebarHelper
 } from "./tests-helpers";
 import { Logger } from "./utils/logger";
 
@@ -18,8 +18,8 @@ export interface Configuration {
 }
 
 export async function TestsGenerator(testsCasesPath: string): Promise<void> {
-    const defaultTemplatePath = FixSep(path.join(testsCasesPath, DEFAULT_TEMPLATE_FILE_NAME));
-    if (await fs.pathExists(defaultTemplatePath)) {
+    const defaultTemplatePath = FixSep(path.join(testsCasesPath, "..", DEFAULT_TEMPLATE_FILE_NAME));
+    if (!await fs.pathExists(defaultTemplatePath)) {
         Logger.Error(`Not found package default template. Expected file location: "${defaultTemplatePath}"`);
         return;
     }
@@ -34,6 +34,8 @@ export async function TestsGenerator(testsCasesPath: string): Promise<void> {
     const targetDirectory = path.join(testsCasesPath, GENERATED_TESTS_DIR_NAME);
     await fs.ensureDir(targetDirectory);
 
+    RegisterJSONStringifyHandlebarHelper();
+
     for (const caseDirPath of casesDirPaths) {
         const { name } = path.parse(caseDirPath);
 
@@ -44,34 +46,18 @@ export async function TestsGenerator(testsCasesPath: string): Promise<void> {
         } else {
             templatePath = defaultTemplatePath;
         }
-        console.log(name, templatePath);
 
         const testConfigPath = FixSep(path.join(caseDirPath, TESTS_CONFIG_FILE_NAME));
         const testConfig = await fs.readJSON(testConfigPath) as Configuration;
 
-        // TODO: add data to 'testDescribe' from 'templatePath' and fill template with data from 'testConfig'
-        const testDescribe = [
-            `import { Generator } from "@src/generator";`,
-            `import { GeneratorConfigurationBuilder } from "@src/builders/generator-configuration-builder";`,
-            "",
-            `test("${name}", async done => {`,
-            Tab(1) + `const projectDirectory = "${caseDirPath}";`,
-            Tab(1) + `const entryFiles  = ${JSON.stringify(testConfig.EntryFiles)};`,
-            "",
-            Tab(1) + "try {",
-            Tab(2) + "const configuration = await new GeneratorConfigurationBuilder(projectDirectory)",
-            Tab(3) + ".Build(entryFiles);",
-            "",
-            Tab(2) + "const generator = new Generator(configuration);",
-            "",
-            Tab(2) + "expect(generator.OutputData).toMatchSnapshot();",
-            Tab(2) + "done();",
-            Tab(1) + "} catch (error) {",
-            Tab(2) + "done.fail(error);",
-            Tab(1) + "}",
-            "});",
-            ""
-        ].join(os.EOL);
+        const source = await fs.readFile(templatePath, "utf8");
+        const template = handlebars.compile(source);
+        const data = {
+            "caseName": name,
+            "projectDirectory": caseDirPath,
+            "entryFiles": testConfig.EntryFiles
+        };
+        const testDescribe = template(data);
 
         const targetFilePathName = path.join(targetDirectory, `${name}.test.ts`);
         await fs.writeFile(targetFilePathName, testDescribe);
