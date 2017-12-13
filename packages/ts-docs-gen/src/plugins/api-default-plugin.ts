a2d1e202ea8b49b20d004857b72904076210e765
Martynas Zilinskas <martiogalaltu@gmail.com>
2017-12-13 17:48:04 +0200

    Updated default plugin.

--- packages/ts-docs-gen/src/plugins/api-default-plugin.ts
+++ packages/ts-docs-gen/src/plugins/api-default-plugin.ts
@@ -1,28 +1,36 @@
+import { Contracts } from "ts-extractor";
 import { MarkdownGenerator } from "@simplrjs/markdown";
+import { Plugin, PluginOptions, PluginResult, PluginSupportedApiItemKindType, PluginHeading } from "../contracts/plugin";
+import { GeneratorHelpers } from "../generator-helpers";
 
-import { ApiItemPluginBase } from "../abstractions/api-item-plugin-base";
-import { SupportedApiItemKindType } from "../contracts/supported-api-item-kind-type";
-import { RenderItemOutputDto } from "../contracts/render-item-output-dto";
-import { PluginData } from "../contracts/plugin-data";
-
-export class ApiDefaultPlugin extends ApiItemPluginBase {
-    public SupportedApiItemKinds(): SupportedApiItemKindType[] {
-        return [this.ApiItemKinds.Any];
+export class ApiDefaultPlugin implements Plugin<Contracts.ApiItemDto> {
+    public SupportedApiItemKinds(): PluginSupportedApiItemKindType[] {
+        return [GeneratorHelpers.ApiItemKinds.Any];
     }
 
-    public Render(data: PluginData): RenderItemOutputDto {
-        const [, alias] = data.Reference;
-        const heading = `${data.ApiItem.ApiKind}: ${alias}`;
-        const output: string[] = [
+    public CheckApiItem(item: Contracts.ApiItemDto): boolean {
+        return true;
+    }
+
+    public Render(data: PluginOptions<Contracts.ApiItemDto>): PluginResult {
+        const heading = `${data.ApiItem.ApiKind}: ${data.Reference.Alias}`;
+        const headings: PluginHeading[] = [
+            {
+                ApiItemId: data.Reference.Id,
+                Heading: heading
+            }
+        ];
+
+        const result: string[] = [
             MarkdownGenerator.Header(heading, 2)
         ];
 
         return {
-            Heading: heading,
+            Headings: headings,
+            Reference: data.Reference,
             ApiItem: data.ApiItem,
-            ParentId: data.ParentId,
-            References: [],
-            RenderOutput: output
+            UsedReferences: [],
+            Result: result,
         };
     }
 }
