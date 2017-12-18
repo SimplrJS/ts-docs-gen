a3c36c5bd6c71b22615d6b585c4e1148eac747f6
Donatas Bražionis <donatas@quatrodev.com>
2017-12-18 13:48:34 +0200

    Removed unused code.

--- tools/test-generator-cli/src/tests-helpers.ts
+++ tools/test-generator-cli/src/tests-helpers.ts
@@ -1,18 +1,8 @@
 import * as path from "path";
 import * as handlebars from "handlebars";
 
-const TAB_STRING = "    ";
-
 export function FixSep(pathname: string): string {
     return pathname.split(path.sep).join("/");
-}
-
-export function Tab(size: number = 1): string {
-    let result: string = "";
-    for (let i = 0; i < size; i++) {
-        result += TAB_STRING;
-    }
-    return result;
 }
 
 export function RegisterJSONStringifyHandlebarHelper(): void {
