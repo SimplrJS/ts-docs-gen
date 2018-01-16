624c576c70b063f8534eee94459a281657f4936b
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-16 16:44:47 +0200

    Added ApiTypeAlias.

---
+++ packages/ts-docs-gen/src/api-items/api-type-alias.ts
@@ -0,0 +1,19 @@
+import { Contracts } from "ts-extractor";
+import { ApiBase } from "./api-base";
+import { GeneratorHelpers } from "../generator-helpers";
+
+export class ApiTypeAlias extends ApiBase<Contracts.ApiTypeAliasDto> {
+    public ToStringArray(alias?: string): string[] {
+        const name = alias || this.Data.Name;
+        const typeParameters = this.GetTypeParameters(this.Data.TypeParameters);
+        const type = GeneratorHelpers.ApiTypeToString(this.ExtractedData, this.Data.Type);
+
+        return [
+            `type ${name}${typeParameters} = ${type}`
+        ];
+    }
+
+    public ToSimpleString(): string {
+        return this.Data.Name;
+    }
+}
