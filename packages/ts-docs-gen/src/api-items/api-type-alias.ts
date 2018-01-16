4b7c4f5f69c0b613eea42cf55fb0ff21cabaf992
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-16 17:03:09 +0200

    Fixed TypeParameters error in ApiTypeAlias.

--- packages/ts-docs-gen/src/api-items/api-type-alias.ts
+++ packages/ts-docs-gen/src/api-items/api-type-alias.ts
@@ -5,7 +5,7 @@ import { GeneratorHelpers } from "../generator-helpers";
 export class ApiTypeAlias extends ApiBase<Contracts.ApiTypeAliasDto> {
     public ToStringArray(alias?: string): string[] {
         const name = alias || this.Data.Name;
-        const typeParameters = this.GetTypeParameters(this.Data.TypeParameters);
+        const typeParameters = this.GetTypeParameters(this.Data);
         const type = GeneratorHelpers.ApiTypeToString(this.ExtractedData, this.Data.Type);
 
         return [
