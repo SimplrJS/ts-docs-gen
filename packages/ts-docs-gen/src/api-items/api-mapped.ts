da41ac0cef97a6021af3619be067e1a2541370ea
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-16 18:30:21 +0200

    Added ApiMapped.

---
+++ packages/ts-docs-gen/src/api-items/api-mapped.ts
@@ -0,0 +1,38 @@
+import { Contracts } from "ts-extractor";
+
+import { BaseApiItem } from "../abstractions/base-api-item";
+import { GeneratorHelpers } from "../generator-helpers";
+import { ApiTypeParameter } from "./api-type-parameter";
+
+export class ApiMapped extends BaseApiItem<Contracts.ApiMappedDto> {
+    protected GetTypeParameter(): ApiTypeParameter | undefined {
+        if (this.Data.TypeParameter == null) {
+            return undefined;
+        }
+
+        const apiItem = this.ExtractedData.Registry[this.Data.TypeParameter] as Contracts.ApiTypeParameterDto;
+        return new ApiTypeParameter(this.ExtractedData, apiItem);
+    }
+
+    public ToStringArray(): string[] {
+        const readonly = this.Data.IsReadonly ? "readonly " : "";
+        const optional = this.Data.IsOptional ? "?" : "";
+
+        const typeParameter = this.GetTypeParameter();
+        let typeParameterString: string;
+        if (typeParameter != null) {
+            typeParameterString = typeParameter.ToString();
+        } else {
+            // TODO: Add logger for missing TypeParameter.
+            typeParameterString = "???";
+        }
+
+        const type = GeneratorHelpers.ApiTypeToString(this.ExtractedData, this.Data.Type);
+
+        return [`{${readonly}[${typeParameterString}]${optional}: ${type}}`];
+    }
+
+    public ToSimpleString(): string {
+        return this.Data.Name;
+    }
+}
