12e544f17c0c42085e5a04ba10a472eba45624e3
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-18 12:17:39 +0200

    Fixed ApiTypeAlias (added TypeParameters).

--- packages/ts-docs-gen/src/api-items/definitions/api-type-alias.ts
+++ packages/ts-docs-gen/src/api-items/definitions/api-type-alias.ts
@@ -1,12 +1,26 @@
-import { Contracts } from "ts-extractor";
+import { Contracts, ExtractDto } from "ts-extractor";
 
 import { ApiDefinitionWithType } from "../api-definition-with-type";
+import { ApiItemReference } from "../../contracts/api-item-reference";
+import { ApiTypeParameter } from "./api-type-parameter";
 
 export class ApiTypeAlias extends ApiDefinitionWithType<Contracts.ApiTypeAliasDto> {
+    constructor(extractedData: ExtractDto, apiItem: Contracts.ApiTypeAliasDto, reference: ApiItemReference) {
+        super(extractedData, apiItem, reference);
+
+        this.typeParameters = this.GetTypeParameters(this.Data);
+    }
+
+    private typeParameters: ApiTypeParameter[];
+
+    public get TypeParameters(): ApiTypeParameter[] {
+        return this.typeParameters;
+    }
+
     public ToText(): string[] {
         const name = this.Reference.Alias || this.Data.Name;
-        const typeParameters = this.GetTypeParameters(this.Data);
         const type = this.SerializedTypeToString(this.Type);
+        const typeParameters = this.TypeParametersToString(this.TypeParameters);
 
         return [
             `type ${name}${typeParameters} = ${type}`
