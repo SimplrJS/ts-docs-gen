90a10cf8322464da195f06cca862139f35098ebb
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-01-26 17:52:41 +0200

    Added support for ImportNamespace and ConstructorType.

--- packages/ts-docs-gen/src/api-items/definitions/api-construct.ts
+++ packages/ts-docs-gen/src/api-items/definitions/api-construct.ts
@@ -4,9 +4,18 @@ import { ReferenceRenderHandler } from "../../contracts/serialized-api-item";
 
 export class ApiConstruct extends ApiCallable<Contracts.ApiConstructDto> {
     public ToText(render: ReferenceRenderHandler = this.DefaultReferenceRenderer): string[] {
-        return [
-            `new ${this.CallableToString(render)};`
-        ];
+        switch (this.ApiItem.ApiKind) {
+            case Contracts.ApiDefinitionKind.Construct: {
+                return [
+                    `new ${this.CallableToString(render)};`
+                ];
+            }
+            case Contracts.ApiDefinitionKind.ConstructorType: {
+                return [
+                    `new ${this.CallableToString(render, " => ")}`
+                ];
+            }
+        }
     }
 
     public ToHeadingText(): string {
